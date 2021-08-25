daf18a1f35e38be91cf174d81ff362abaa648f99
21grahams <graham.kirsh@gmail.com>
2021-08-24 22:47:27 -0600

    added reading_test.js file for mocha to test the .find() functionality of Mongo to find all users with the name joe

--- test/create_test.js
+++ test/create_test.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
-const User = require('../src/user.js');
+const User = require('../src/user');
 
 describe('Creating records', () => {
   it('saves a user', () => {
-    const joe = new User({ name: 'Joe' })
+    const joe = new User({ name: 'joe' })
 
-    joe.save();
+    joe.save()
+      .then(() => {
+        // Has joe been saved successfully?
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -4,22 +4,25 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/mongoCRUD_test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection
-  .once("open", () => console.log("Good to go!"))
-  .on("error", (error) => {
-    console.warn("Warning", error);
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect("mongodb://localhost/mongoCRUD_test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
-
-
-  // hook - function that gets executed before any testing function gets executed inside test suite
-
-  beforeEach((done) => {
-    mongoose.connection.collections.users.drop(() => {
-      // Ready to run the next test
-      done();
+  mongoose.connection
+    .once("open", () => { done(); })
+    .on("error", (error) => {
+      console.warn("Warning", error);
     });
-  })
+});
+
+// hook - function that gets executed before any testing function gets executed inside test suite
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test
+    done();
+  });
+});
