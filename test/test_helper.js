1c2e2c93d882163f5f78521e4286e4df3a0c20a4
21grahams <graham.kirsh@gmail.com>
2021-08-24 21:49:38 -0600

    added single test in create_test.js using mocha that currently tests that a new user instance was created and saved to db. Need to still add functionality to test to finalize

--- test/test_helper.js
+++ test/test_helper.js
@@ -13,3 +13,13 @@ mongoose.connection
   .on("error", (error) => {
     console.warn("Warning", error);
   });
+
+
+  // hook - function that gets executed before any testing function gets executed inside test suite
+
+  beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+      // Ready to run the next test
+      done();
+    });
+  })
